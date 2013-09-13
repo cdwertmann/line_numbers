92cfd9cbc90c2ebad288f8b3c8cfb5b62412e857
Christoph Dwertmann <cdwertmann@gmail.com>
2013-09-13 13:20:14 +0200

    Fixes #3, adds backward compatibility for Redmine < 2.3.3

--- lib/monkey_patch.rb
+++ lib/monkey_patch.rb
@@ -1,13 +1,11 @@
 # patch to enable CodeRay line numbering
 module Redmine
   module SyntaxHighlighting
-    module CodeRay
-      class << self
-        # Highlights +text+ using +language+ syntax
-        # Should not return outer pre tag
-        def highlight_by_language(text, language)
-          ::CodeRay.scan(text, language).html(:wrap => :span, :line_numbers => :inline)
-        end
+    class << self
+      # Highlights +text+ using +language+ syntax
+      # Should not return outer pre tag
+      def highlight_by_language(text, language)
+        ::CodeRay.scan(text, language).html(:wrap => :span, :line_numbers => :inline)
       end
     end
   end
